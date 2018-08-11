a0bd62d3301128eb0600feb12518c95ec3eaaa0a
Aneel Marshall <aneel.marshall@outlook.com>
2018-08-11 20:55:22 +0100

    Add #broken? and #report_broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,12 @@ describe Bike do
   it "responds to method working?" do
     expect(subject).to respond_to(:working?)
   end
+
+  it "allows reporting bike as broken" do
+    expect(subject).to respond_to(:report_broken)
+  end
+
+  it "#broken? tells you if bike is broken, returns boolean" do
+    expect(subject.broken?).to be false  
+  end
 end
