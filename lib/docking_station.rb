3860e9b8bea257754b97599a369ea8ee9470d058
Aneel Marshall <aneel.marshall@outlook.com>
2018-11-09 15:04:21 +0000

    refactor: remove magic numbers in tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,16 +12,26 @@ class DockingStation
 
   def dock(bike)
     # guard condition
-    raise("Docking Station Full") if bikes.length >= DEFAULT_CAPACITY
+    raise("Docking Station Full") if full?
     @bikes << bike
     bike  
   end
 
   def release_bike
     # guard condition
-    raise("There are no bikes") if bikes.empty? 
+    raise("There are no bikes") if empty? 
     # raise("Broken bike") if docked_bikes.last.broken?
     bikes.pop 
+  end
+
+  private
+
+  def full? 
+    bikes.length >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    bikes.empty?
   end
 
   # def dock_broken_bike(bike)
